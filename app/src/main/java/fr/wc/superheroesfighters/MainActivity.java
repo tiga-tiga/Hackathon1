59a9b7ef861a1ddd8088bda943d716042c098da7
Hedi Benaissa <h_benaissa@hotmail.fr>
2018-04-05 21:21:40 +0200

    title

--- app/src/main/java/fr/wc/superheroesfighters/MainActivity.java
+++ app/src/main/java/fr/wc/superheroesfighters/MainActivity.java
@@ -2,6 +2,9 @@ package fr.wc.superheroesfighters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = findViewById(R.id.imageButton2);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
