acc127c297786410f147d05fa0fd16fed411368f
Hedi Benaissa <h_benaissa@hotmail.fr>
2018-04-06 05:23:09 +0200

    designfin

--- app/src/main/java/fr/wc/superheroesfighters/MainActivity.java
+++ app/src/main/java/fr/wc/superheroesfighters/MainActivity.java
@@ -1,39 +1,34 @@
 package fr.wc.superheroesfighters;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        ImageView img = findViewById(R.id.imageButton2);
+        ImageView img = findViewById(R.id.imageButton9);
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent bou = new Intent(MainActivity.this, SelectEnemyActivity.class);
+                startActivity(bou);
 
             }
         });
 
-
-        Button bouton = findViewById(R.id.bouton);
-
-                bouton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent bou = new Intent(MainActivity.this, SelectEnemyActivity.class);
-                        startActivity(bou);
-                    }
-                });
 
     }
 }
